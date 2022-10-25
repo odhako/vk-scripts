a71490c59e8396732fa8cac57b94d5a4c05caf0f
odhako <odhako@gmail.com>
2022-10-25 18:36:57 +0400

    Enable scheduler

--- vk_scripts/reposter/apps.py
+++ vk_scripts/reposter/apps.py
@@ -5,6 +5,6 @@ class ReposterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vk_scripts.reposter'
 
-    # def ready(self):
-    #     from .scheduler import scheduler
-    #     scheduler.start()
+    def ready(self):
+        from .scheduler import scheduler
+        scheduler.start()
