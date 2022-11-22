fda5e4f95baefe711c1f3c37195110d70e282fd6
odhako <odhako@gmail.com>
2022-11-22 21:50:18 +0400

    Version 1.1: remove old scheduler

--- vk_scripts/reposter/apps.py
+++ vk_scripts/reposter/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ReposterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vk_scripts.reposter'
-
-    def ready(self):
-        from .scheduler import scheduler
-        scheduler.start()
