7ee31534f293add023351018cf0f4c7272a7d69e
odhako <odhako@gmail.com>
2023-02-09 15:52:15 +0400

    Clean up

--- vk_scripts_old_ver/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class AddGroupForm(forms.Form):
-    group_url = forms.CharField(label='Add group', max_length=100)
-
-
-class GroupsForm(forms.Form):
-    group = forms.BooleanField
